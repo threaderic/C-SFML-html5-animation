f6084fcf56054724d0f0d6deac796b3b8cc6ef4d
threaderic <52260863+threaderic@users.noreply.github.com>
2019-07-08 00:31:36 +0200

    add Scons build facility - begin for 4 first folders

--- README.md
+++ README.md
@@ -18,5 +18,20 @@ Folder Structure:
 Note 1: If you want to build any example, just run CMake on their own root folder.
 
 Note 2: I have built all the examples on Windows with CMake; eventually will try to add Linux and OSx support, but I'll be more than happy to recieve any help with this in the meantime :)
+--> see below : a newbie try to help for linux users :)
 
 Note 3: Example ch04-12-video-frames uses Visual Studio temporarily.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+for linux user
+SFML Version: 2.4.2
+
+State: In-Progress.
+
+build total folder with scons
+1. install scons
+2. open terminal in the root folder of this project (same as README.md)
+3. tape "scons" <enter>
+4. go to the build-scons folder/<folder of desired example>/bin/
+5. tape "./<name of the program>
+6. enjoy ;)
--- ch02-03-mouse-events/src/main.cpp
+++ ch02-03-mouse-events/src/main.cpp
@@ -1,4 +1,9 @@
-#include "SFML\Graphics.hpp"
+#if _WIN32 //for both 32 and 64 bit environnemnts -- Pre-defined Compiler Macros -- sourceforge
+    #include "SFML\Graphics.hpp"
+#elif __linux__
+    #include <SFML/Graphics.hpp>
+#endif
+
 #include <iostream>
 int main() 
 {
@@ -18,6 +23,42 @@ int main()
 					break;
 				case sf::Event::MouseButtonPressed:
 					std::cout << "Mouse Down\n";
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                    {
+                        // left click..
+                        std::cout << "left click\n";
+                    }
+                    else if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+                    {
+                        // right click..
+                        std::cout << "right click\n";
+                    }
+                    else
+                    {
+                        // probably middle clicks but who knows ;)
+                        std::cout << "on which fucking button did you clicked ?\n";
+                    }
+                    
+                    //next code to be improved...
+					if (clickClock.restart().asMilliseconds() <= 500) 
+					{
+						std::cout << "Double Click\n";
+                        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                    {
+                        // left click..
+                        std::cout << "double left click\n";
+                    }
+                    else if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+                    {
+                        // right click..
+                        std::cout << "double right click\n";
+                    }
+                    else
+                    {
+                        // probably middle clicks but who knows ;)
+                        std::cout << "on which fucking button did you double clicked ?\n";
+                    }
+                    }
 					break;
 				case sf::Event::MouseButtonReleased:
 					std::cout << "Mouse Up\n";
